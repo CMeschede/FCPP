eb7461474c20e394a83effa7e1aec71bcbd2d2ca
CMeschede <christina.meschede@tu-dortmund.de>
2022-11-02 11:46:33 +0100

    updating the initialisation of rho/sigma

--- R/optimation_functions.R
+++ R/optimation_functions.R
@@ -17,7 +17,7 @@ optim_multistart_bt <- function(WW, distance_fct, ptail, rho,
                          tryCatch(
                            optim( par = c(t., e.), fn = function(x) {
                              do.call(
-                               distance_fct, list(WW = WW, tail = x[1], ei = x[2], 
+                               distance_fct, list(WW = WW, tail = x[1], ei = x[2],
                                                   scale = ptail ^ {-1 / x[1]} * rho)
                              )
                            },
@@ -38,29 +38,37 @@ optim_multistart_bt <- function(WW, distance_fct, ptail, rho,
   return(est2)
 }
 
-optim_multistart_btr <- function(WW, distance_fct, ptail, type = 1,
-                                            t = c(0.25,0.55,0.85), e = c(0.25,0.55,0.85), r = NULL, s = NULL,
+
+#' @export
+optim_multistart_btr <- function(WW, distance_fct, ptail = NULL, type = 1,
+                                            t = c(0.25,0.55,0.85), e = c(0.25,0.55,0.85),
+                                            r = NULL, s = NULL,
                                             a_tail = 0.1, a_ei = 0.1, method = "L-BFGS-B") {
   if(is.data.frame(WW) & length(WW) == 1) {
     WW <- dplyr::pull(WW)
   }
   initial <- MittagLeffleR::logMomentEstimator(WW) # for the starting point of rho
   if(type == 1) {
+  if(is.null(ptail)) stop("for type = 1 you need to give the tail probability ptail")
   start <- tidyr::crossing(
     t = t,
     e = e
   )
-  if(is.null(r)) {
-    start <- start |> tidyr::crossing(
-      r = initial[2] * ptail ^ {1 / t}
-    )
-  } else {
+  if(is.numeric(r)) {
     start <- start |> tidyr::crossing(
       r = r
     )
+  } else if(is.numeric(s)) {
+    start <- start |> tidyr::crossing(
+      r = s * ptail ^ {1 / t}
+    )
+  } else {
+    start <- start |> tidyr::crossing(
+      r = initial[2] * ptail ^ {1 / t}
+    )
   }
-    
-  
+
+
   # para_orig: parameter (tail, ei, scale)
   # para: a transformed parameter so that the optimation is unconstrained
   #para_orig <- function(para) {c()}
@@ -69,7 +77,7 @@ optim_multistart_btr <- function(WW, distance_fct, ptail, type = 1,
                          tryCatch(
                            optim( par = c(t., e., r.), fn = function(x) {
                              do.call(
-                               distance_fct, list(WW = WW, tail = x[1], ei = x[2], 
+                               distance_fct, list(WW = WW, tail = x[1], ei = x[2],
                                                   scale = ptail ^ {-1 / x[1]} * x[3])
                              )
                            },
@@ -92,16 +100,20 @@ optim_multistart_btr <- function(WW, distance_fct, ptail, type = 1,
   return(est2)
   }
   if(type == 2) {
-    if(is.null(r)) {
-      start <- start |> tidyr::crossing(
-        s = initial[2]
-      )
-    } else {
+    if(is.numeric(s)) {
       start <- start |> tidyr::crossing(
         s = s
       )
+    } else if(is.numeric(r) & is.numeric(ptail) & length(ptail) == 1) {
+      start <- start |> tidyr::crossing(
+        s = r * ptail ^ {-1 / t}
+      )
+    } else {
+      start <- start |> tidyr::crossing(
+        s = initial[2]
+      )
     }
-    
+
     # para_orig: parameter (tail, ei, scale)
     # para: a transformed parameter so that the optimation is unconstrained
     #para_orig <- function(para) {c()}
@@ -134,7 +146,7 @@ optim_multistart_btr <- function(WW, distance_fct, ptail, type = 1,
     return(est2)
   }
 }
-                                              
+
 # WW - vector of inter-exceedance times
 # ptail - P(JJ > u) = k/n (number of exceedances over number of observations)
 # distance_fct - r-function calculating the distance between the asymptotic and empirical cdf
