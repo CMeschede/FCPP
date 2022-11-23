b13ebe8f1d806c985862662a5039d73be45d76f2
CMeschede <christina.meschede@tu-dortmund.de>
2022-11-23 18:13:42 +0100

    update of the documentation and inclusion of a scale parameter

--- R/datagenerating_functions.R
+++ R/datagenerating_functions.R
@@ -1,46 +1,77 @@
 #' Data Generating Function
 #'
-#' Function that generates a marked point process (JJ[i], WW[i]) \eqn{i = 1,...,n}.
-#' The JJ[i] is the i-th mark and WW[i] is the waiting time between the i-1- and
-#' i-th event.
+#' Function that generates a marked point process \eqn{(JJ[i], WW[i]), i = 1,...,n}.
+#' The JJ[i] is the i-th mark and WW[i] is the waiting time between the (i-1)-th and
+#' i-th event. The marks (JJ[i]) form a MAR-process which is a stationary time series
+#' with extremal index \eqn{\theta \in (0,1]}.
+#' The generated process fulfills all assumptions so that the inter-exceedance times (IETs)
+#' are asymptotically mixture distributed with the dirac measure at point zero and the
+#' Mittag-Leffler distribution as parts.
+#' The IETs are the times between two consequtive extremes which are identified
+#' by the peak-over-threshold (POT) method.
 #'
 #' @param n number of observations
-#' @param ei true parameter theta in (0,1] (extremal index)
-#' @param stability The stability parameter of the distribution of the waiting times.
-#' The stability parameter is in \eqn{\mathbb{R}^+\setminus\lbrace 1\rbrace} if \code{wait_dist}
-#' is "pareto" and in (0,1] else.
-#' @param wait_dist distribution of waiting times. Should be "stable",
-#' "ML" (Mittag-Leffler Distribution) or "pareto".
+#' @param ei extremal index \eqn{\theta \in (0,1]}
+#' @param stability stability parameter \eqn{\alpha > 0}
+#' used for the waiting time distribution (see 'Details')
+#' @param scale scale parameter \eqn{\sigma > 0}. Default \code{scale = 1}.
+#' @param wait_dist distribution of the waiting times WW (see 'Details')
 #'
 #' @details
-#' If the distribution of the waiting times is chosen as "stable" and the stability parameter
-#' is smaller than 1, then the waiting times are stable distributed.
-#' In this case \code{WW} are generated using the
-#' package \code{stabledist}. In the special case of \code{stability}=1, the stable distribution
-#' simplifies to a dirac measure in 1.
+#' The event magnitudes/ marks (JJ) form a Max-Autoregression(MAR)-process.
+#' This process is stationary with extremal index \eqn{\theta}.
 #'
-#' For the case of \code{wait_dist="ML"} a Mittag-Leffler distribution where the
-#' tail parameter and the stability parameter are the same. If the stability parameter
-#' is equal to 1, then the Mittag-Leffler distribution corresponds to an exponential
-#' distribution with
-#' rate 1. For the data generation of the Mittag-Leffler distribution
+#' The waiting times (WWs) are i.i.d. and stochastically independent to the
+#' event magnitudes (JJ).
+#' The waiting time distribution can be chosen as "\code{stable}", "\code{ML}" or
+#' "\code{pareto}".
+#'
+#' If \code{wait_dist = "stable"}, the \code{stability} parameter \eqn{\alpha} has to be in
+#' the interval (0,1].
+#' In case of \eqn{\alpha < 1}, the WWs are stable distributed with stability parameter \eqn{\alpha}.
+#' Then, the distribution is heavy-tailed in the sense that mean and variance do
+#' not exist.
+#' In this case \code{WW} are generated using the package \code{stabledist}.
+#' In the special case of \code{stability = 1}, the waiting times are equidistant equal \eqn{\sigma},
+#' since the stable distribution converges in distribution to the degenerative dirac measure in \eqn{\sigma}.
+#'
+#' If \code{wait_dist = "ML"}, the \code{stability} parameter \eqn{\alpha} has to be in
+#' the interval (0,1].
+#' The WWs are Mittag-Leffler distributed with stability/tail parameter \eqn{\alpha}.
+#' For \eqn{\alpha < 1} the distribution is heavy-tailed in the sense that mean
+#' and variance do not exist.
+#' In case of \eqn{\alpha = 1}, the Mittag-Leffler distribution corresponds to
+#' an exponential distribution with rate \eqn{1/\sigma}.
+#' For the data generation of the Mittag-Leffler distribution
 #' the package \code{MittagLeffleR} is used.
 #'
-#' For \code{wait_dist="pareto"} and a stability parameter
-#' \eqn{\alpha} smaller than 1, a Pareto distribution
-#'  with shape parameter \eqn{\alpha} and scale parameter
-#'  \eqn{\big(1/\Gamma(1-\alpha)\big)^{1/\alpha}} is used
-#'  to generate the waiting times. For \eqn{\alpha} larger than 1 the
-#'  scale parameter becomes \eqn{(\alpha-1)/\alpha}.For the
-#'  generation of the waiting times the Package \code{ReIns} is used.
+#' If \code{wait_dist = "pareto"}, the \code{stability} parameter \eqn{\alpha}
+#' has to be in \eqn{(0, \infty)/{1}}.
+#' If \eqn{\alpha < 1}, a Pareto distribution with shape parameter \eqn{\alpha}
+#' and scale parameter \eqn{(1/(\Gamma(1-\alpha))^{1/\alpha} * \sigma} is used.
+#' Then the distribution is heavy-tailed in the sense that mean and variance do not exist.
+#' If \eqn{\alpha > 1}, the scale parameter becomes \eqn{(\alpha-1)/\alpha * \sigma}.
+#' For \eqn{\alpha > 1}, the expected value exists and equals \eqn{\sigma}. For \eqn{\alpha > 2}
+#' the variance exists additionally.
+#' For the generation of the Pareto distribution the Package \code{ReIns} is used.
 #'
+#' For all three waiting time distributions with \code{stability} parameter \eqn{\alpha < 1}
+#' it holds that they are in the domain of attraction of the stable distribution
+#' which is a positively skewed sum-stable distribution.
+#'
+#' The generated process (JJ,WW) fulfills all assumptions so that the inter-exceedance times (IETs)
+#' are asymptotically mixture distributed with the dirac measure at point zero and the
+#' Mittag-Leffler distribution as parts.
+#' The IETs are the times between two consequtive extremes which are identified
+#' by the peak-over-threshold (POT) method.
 #'
 #' @return
-#' A tibble with
+#' A tibble with two columns
 #'
-#' \code{JJ} a stationary MAR-process with extremal index \eqn{\theta} (ei).
+#' \code{JJ} a stationary MAR-process with extremal index \code{ei}
 #'
-#' \code{WW} independent waiting times between the events.
+#' \code{WW} independent waiting times with stability parameter \code{stability}
+#' and scale parameter \code{scale}
 #'
 #' @export
 #'
@@ -51,7 +82,7 @@
 #' dat2
 
 
-data_generation <- function(n, ei = 1, stability = 1,
+data_generation <- function(n, ei = 1, stability = 1, scale = 1,
                             wait_dist = "stable") {
   ## input control:
   # 'n' number of observations
@@ -118,7 +149,7 @@ if(wait_dist == "stable") { # stable
   }
 
 
-    if(wait_dist == "ML") { # Mittag-Leffler
+    if(wait_dist == "ML") { # Mittag-Leffler distribution
      # stability = tail < 1
        sigma <- 1
 
@@ -130,5 +161,6 @@ if(wait_dist == "stable") { # stable
        W <- stats::rexp(n, rate = 1)
      }
   }
+  W <- scale * W
   return(tibble::tibble(JJ = J, WW = W))
 }
